2b97ea8b9415293f4de3622bb16bd1431bda49d4
Martin Gausby <martin@gausby.dk>
2016-01-31 22:23:40 +0100

    Initial commit
      - Should be able to decode Magnet URI's.
      - Should support number suffixes on keys, such as tr.1=...&tr.2
      - It has a Magnet struct

---
+++ lib/magnet.ex
@@ -0,0 +1,136 @@
+defmodule Magnet do
+  defstruct(
+    name: nil,
+    length: nil,
+    info_hash: [],
+    fallback: nil,
+    source: [],
+    keywords: [],
+    manifest: nil,
+    announce: []
+  )
+
+  def decode("magnet:?" <> magnet) do
+    do_decode(magnet, [])
+  end
+
+  defp do_decode("", acc), do: acc
+  defp do_decode(rest, acc) do
+    with(
+      {key, rest} <- do_decode_key(rest, []),
+      {value, rest} <- do_decode_value(rest, []),
+      do: do_decode(rest, [{key, value}|acc])
+    )
+  end
+
+  defp do_decode_key(<<"=", rest::binary>>, acc) do
+    result = convert_list_to_string(acc)
+    {result, rest}
+  end
+  defp do_decode_key(<<char, rest::binary>>, acc) do
+    do_decode_key(rest, [char|acc])
+  end
+
+  defp do_decode_value("", acc) do
+    result = convert_list_to_string(acc)
+    {result, ""}
+  end
+  defp do_decode_value(<<"&", rest::binary>>, acc) do
+    result = convert_list_to_string(acc)
+    {result, rest}
+  end
+  defp do_decode_value(<<char, rest::binary>>, acc) do
+    do_decode_value(rest, [char|acc])
+  end
+
+  # turn char lists created by accumulating strings into utf8 strings
+  defp convert_list_to_string(list) do
+    list
+    |> Enum.reverse
+    |> List.to_string
+  end
+end
+
+defimpl Collectable, for: Magnet do
+  def into(original) do
+    {original, fn
+      # ignore entries with empty values
+      acc, {:cont, {_, ""}} ->
+        acc
+
+      # as (Acceptable Source) - Web link to the file online
+      acc, {:cont, {"as", value}} ->
+        uri = URI.decode value
+        %Magnet{acc|fallback: uri}
+
+      # dn (Display Name) - Suggested filename
+      acc, {:cont, {"dn", value}} ->
+        %Magnet{acc|name: value}
+
+      # kt (Keyword Topic) - Key words for the given torrent
+      acc, {:cont, {<<"kt", priority::binary>>, value}} ->
+        entry = parse_suffix_number(priority, String.split(value, "+"))
+        %Magnet{acc|keywords: [entry|acc.keywords]}
+
+      # mt (Manifest Topic) - link to a file that contains a list of magneto (MAGMA - MAGnet MAnifest)
+      acc, {:cont, {"mt", value}} ->
+        %Magnet{acc|manifest: value}
+
+      # tr (address TRacker) - Tracker/Announce URLs for BitTorrent downloads
+      acc, {:cont, {<<"tr", priority::binary>>, value}} ->
+        announce = URI.decode(value)
+        entry = parse_suffix_number(priority, announce)
+        %Magnet{acc|announce: [entry|acc.announce]}
+
+      # xl (eXact Length) - File size in bytes
+      acc, {:cont, {"xl", value}} ->
+        length = String.to_integer(value)
+        %Magnet{acc|length: length}
+
+      # xs (eXact Source) - peer-to-peer links
+      acc, {:cont, {<<"xs", priority::binary>>, value}} ->
+        uri = URI.decode(value)
+        entry = parse_suffix_number(priority, uri)
+        %Magnet{acc|source: [entry|acc.source]}
+
+      # xt (eXact Topic) - URN containing file hash
+      acc, {:cont, {<<"xt", priority::binary>>, value}} ->
+        entry = parse_suffix_number(priority, value)
+        %Magnet{acc|info_hash: [entry|acc.info_hash]}
+
+      acc, :done ->
+        keywords =
+          acc.keywords
+          |> sort_by_priority
+          |> List.flatten
+          |> Enum.dedup
+
+        %Magnet{acc|info_hash: prepare_list(acc.info_hash),
+                    announce: prepare_list(acc.announce),
+                    source: prepare_list(acc.source),
+                    keywords: keywords}
+
+      _, :halt ->
+        :ok
+    end}
+  end
+
+  defp prepare_list(list) do
+    list
+    |> sort_by_priority
+    |> Enum.dedup
+  end
+
+  defp sort_by_priority(priority_list) do
+    priority_list
+    |> Enum.sort_by(&(elem(&1, 0)))
+    |> Enum.map(&(elem(&1, 1)))
+  end
+
+  defp parse_suffix_number("", value),
+    do: {0, value}
+  defp parse_suffix_number(<<".", number::binary>>, value) do
+    {num, _} = Integer.parse(number)
+    {num, value}
+  end
+end
---
+++ test/magnet_test.exs
@@ -0,0 +1,109 @@
+defmodule MagnetTest do
+  use ExUnit.Case
+  doctest Magnet
+
+  test "decode an empty magnet uri" do
+    assert %Magnet{} = Magnet.decode("magnet:?") |> Enum.into(%Magnet{})
+  end
+
+  test "decode a valid magnet uri" do
+    magnet =
+      ~w(magnet:?
+          xt=urn:ed2k:354B15E68FB8F36D7CD88FF94116CDC1
+         &xt=urn:tree:tiger:7N5OAMRNGMSSEUE3ORHOKWN4WWIQ5X4EBOOTLJY
+         &xt=urn:btih:QHQXPYWMACKDWKP47RRVIV7VOURXFE5Q
+         &xl=10826029
+         &dn=mediawiki-1.15.1.tar.gz
+         &tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce
+         &as=http%3A%2F%2Fdownload.wikimedia.org%2Fmediawiki%2F1.15%2Fmediawiki-1.15.1.tar.gz
+         &xs=http%3A%2F%2Fcache.example.org%2FXRX2PEFXOOEJFRVUCX6HMZMKS5TWG4K5
+         &xs=dchub://example.org
+      ) |> Enum.join
+
+    assert %Magnet{name: "mediawiki-1.15.1.tar.gz",
+                   announce: ["udp://tracker.openbittorrent.com:80/announce"],
+                   fallback: "http://download.wikimedia.org/mediawiki/1.15/mediawiki-1.15.1.tar.gz",
+                   info_hash: ["urn:ed2k:354B15E68FB8F36D7CD88FF94116CDC1",
+                               "urn:tree:tiger:7N5OAMRNGMSSEUE3ORHOKWN4WWIQ5X4EBOOTLJY",
+                               "urn:btih:QHQXPYWMACKDWKP47RRVIV7VOURXFE5Q"],
+                   length: 10826029,
+                   source: ["http://cache.example.org/XRX2PEFXOOEJFRVUCX6HMZMKS5TWG4K5",
+                            "dchub://example.org"]
+                  } = Magnet.decode(magnet) |> Enum.into(%Magnet{})
+  end
+
+  test "zero length file" do
+    magnet =
+      ~w(magnet:?
+          xt=urn:ed2k:31D6CFE0D16A9E31B735C9D70EC089C0
+         &xl=0
+         &dn=zero_len.file
+         &xt=urn:bitprint:3I42H3S6NNF2QMSVX7XZKYAYSCX5QBYJ.LWPNAQCDBZRYXW3VJHVCJ64QBZNGHOHHHZWCLNQ
+         &xt=urn:md5:D42D8CD98F00B2049E800989ECF8427E
+      ) |> Enum.join
+
+    assert %Magnet{name: "zero_len.file",
+                   length: 0,
+                   info_hash: [
+                     "urn:ed2k:31D6CFE0D16A9E31B735C9D70EC089C0",
+                     "urn:bitprint:3I42H3S6NNF2QMSVX7XZKYAYSCX5QBYJ.LWPNAQCDBZRYXW3VJHVCJ64QBZNGHOHHHZWCLNQ",
+                     "urn:md5:D42D8CD98F00B2049E800989ECF8427E"]
+                  } = Magnet.decode(magnet) |> Enum.into(%Magnet{})
+
+  end
+
+  test "empty values should not break the decoder" do
+    magnet = "magnet:?xt=&as=&kt=&mt=&tr=&xl=&xs="
+    assert %Magnet{} = Magnet.decode(magnet) |> Enum.into(%Magnet{})
+  end
+
+  test "extracting keywords" do
+    magnet = "magnet:?kt=foo+bar+baz"
+    assert %Magnet{keywords: ["foo", "bar", "baz"]} = Magnet.decode(magnet) |> Enum.into(%Magnet{})
+
+    magnet = "magnet:?kt=foo&kt=bar&kt=baz"
+    assert %Magnet{keywords: ["foo", "bar", "baz"]} = Magnet.decode(magnet) |> Enum.into(%Magnet{})
+
+    magnet = "magnet:?kt=foo"
+    assert %Magnet{keywords: ["foo"]} = Magnet.decode(magnet) |> Enum.into(%Magnet{})
+
+    magnet = "magnet:?kt=foo+foo+foo"
+    assert %Magnet{keywords: ["foo"]} = Magnet.decode(magnet) |> Enum.into(%Magnet{})
+
+    magnet = "magnet:?kt.1=foo+foo+foo&kt.2=bar"
+    assert %Magnet{keywords: ["foo", "bar"]} = Magnet.decode(magnet) |> Enum.into(%Magnet{})
+
+  end
+
+  test "decode entries with dot-number suffixes" do
+    magnet_url = ~w(
+      magnet:?
+       xt.1=urn:sha1:YNCKHTQCBWTRJNIV4WNAE52SJUQCZ5OC
+      &xt.2=urn:sha1:TXGCZTQH2N6L6OUQAJJPFLAHG2LTGBC7
+      ) |> Enum.join
+
+    assert %Magnet{info_hash: ["urn:sha1:YNCKHTQCBWTRJNIV4WNAE52SJUQCZ5OC",
+                               "urn:sha1:TXGCZTQH2N6L6OUQAJJPFLAHG2LTGBC7"]
+                  } = Magnet.decode(magnet_url) |> Enum.into(%Magnet{})
+  end
+
+  test "announce lists should get dedubbed" do
+    magnet = ~w(
+      magnet:?
+      xt=urn:ed2k:3541B56E8FB8F3D76C8DF8F94161CDC1
+      &tr=udp%3A%2F%2Ftracker.example4.com%3A80
+      &tr=udp%3A%2F%2Ftracker.example4.com%3A80
+      &tr=udp%3A%2F%2Ftracker.example5.com%3A80
+      &tr=udp%3A%2F%2Ftracker.example3.com%3A6969
+      &tr=udp%3A%2F%2Ftracker.example2.com%3A80
+      &tr=udp%3A%2F%2Ftracker.example1.com%3A1337
+    ) |> Enum.join
+
+    assert %Magnet{announce: ["udp://tracker.example4.com:80",
+                              "udp://tracker.example5.com:80",
+                              "udp://tracker.example3.com:6969",
+                              "udp://tracker.example2.com:80",
+                              "udp://tracker.example1.com:1337"]
+                  } = Magnet.decode(magnet) |> Enum.into(%Magnet{})
+  end
+end

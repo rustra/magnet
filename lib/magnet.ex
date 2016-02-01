fca711567debddbc6c22fa5332dcccf24f0a4f38
Martin Gausby <martin@gausby.dk>
2016-02-01 21:49:40 +0100

    Restructured the code
    The decoder part now lives in a dedicated decoder module and the main
    module is delegating to it.

--- lib/magnet.ex
+++ lib/magnet.ex
@@ -10,45 +10,7 @@ defmodule Magnet do
     announce: []
   )
 
-  def decode("magnet:?" <> magnet) do
-    do_decode(magnet, [])
-  end
-
-  defp do_decode("", acc), do: acc
-  defp do_decode(rest, acc) do
-    with(
-      {key, rest} <- do_decode_key(rest, []),
-      {value, rest} <- do_decode_value(rest, []),
-      do: do_decode(rest, [{key, value}|acc])
-    )
-  end
-
-  defp do_decode_key(<<"=", rest::binary>>, acc) do
-    result = convert_list_to_string(acc)
-    {result, rest}
-  end
-  defp do_decode_key(<<char, rest::binary>>, acc) do
-    do_decode_key(rest, [char|acc])
-  end
-
-  defp do_decode_value("", acc) do
-    result = convert_list_to_string(acc)
-    {result, ""}
-  end
-  defp do_decode_value(<<"&", rest::binary>>, acc) do
-    result = convert_list_to_string(acc)
-    {result, rest}
-  end
-  defp do_decode_value(<<char, rest::binary>>, acc) do
-    do_decode_value(rest, [char|acc])
-  end
-
-  # turn char lists created by accumulating strings into utf8 strings
-  defp convert_list_to_string(list) do
-    list
-    |> Enum.reverse
-    |> List.to_string
-  end
+  defdelegate decode(data), to: Magnet.Decoder
 end
 
 defimpl Collectable, for: Magnet do
---
+++ lib/magnet/decoder.ex
@@ -0,0 +1,41 @@
+defmodule Magnet.Decoder do
+  def decode("magnet:?" <> magnet) do
+    do_decode(magnet, [])
+  end
+
+  defp do_decode("", acc), do: acc
+  defp do_decode(rest, acc) do
+    with(
+      {key, rest} <- do_decode_key(rest, []),
+      {value, rest} <- do_decode_value(rest, []),
+      do: do_decode(rest, [{key, value}|acc])
+    )
+  end
+
+  defp do_decode_key(<<"=", rest::binary>>, acc) do
+    result = convert_list_to_string(acc)
+    {result, rest}
+  end
+  defp do_decode_key(<<char, rest::binary>>, acc) do
+    do_decode_key(rest, [char|acc])
+  end
+
+  defp do_decode_value("", acc) do
+    result = convert_list_to_string(acc)
+    {result, ""}
+  end
+  defp do_decode_value(<<"&", rest::binary>>, acc) do
+    result = convert_list_to_string(acc)
+    {result, rest}
+  end
+  defp do_decode_value(<<char, rest::binary>>, acc) do
+    do_decode_value(rest, [char|acc])
+  end
+
+  # turn char lists created by accumulating strings into utf8 strings
+  defp convert_list_to_string(list) do
+    list
+    |> Enum.reverse
+    |> List.to_string
+  end
+end
